023b05953c338047c535249180ff36682f8f2261
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-16 14:25:27 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,18 +10,16 @@
 int main(void)
 
 {
-	char letter = 'a';
-	
-	while (letter <= 'z')
+	char ch = 'a';
+
+	while (ch <= 'z')
 	{
-		if ((letter != 'q') && (letter != 'e'))
+		if ((ch != 'e') && (ch != 'q'))
 		{
-			putchar(letter);
+			putchar(ch);
 		}
-		letter++;
+		ch++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
