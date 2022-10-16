6fa03d9d0db7ae4a2f3236134cc439d3f08ecda4
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-16 13:54:44 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets both except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		if ((ch != 'q') && (ch != 'e'))
+		{
+			putchar(ch);
+		}
+		ch++;
+
+	}
+	putchar('\n');
+	return (0);
+}
