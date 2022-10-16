4a20e74244a5188726a7e4c20129010968f16707
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-16 14:32:56 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
