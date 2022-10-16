9b3e486730457d495f51ae583f0d95afc380319e
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-16 15:04:14 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-	int num = '0';
-	char letter = 'a';
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
 
 	while (num <= '9') /*Print 0-9*/
 	{
