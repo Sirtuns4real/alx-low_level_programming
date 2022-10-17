8b7f575991074681ce1699b3760f70ab325f8681
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-17 05:22:46 -0700

    ADV 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  prints all possible different combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int hundreds;
+
+	for (hundreds ='0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) || 
+							(tens < ones) || (hundreds > tens))) /*eliminates repitition*/
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones =='9' && hundreds == '7' && 
+								tens == '8')) /*adds commas and spaces*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}		
+
