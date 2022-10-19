de8d74423aec2dc8f2043cc346337eb6cd94febb
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-19 09:21:24 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers to 98
+ *
+ * @n: number provided by user
+ */
+
+void print_to_98(int n)
+
+{
+	for (n = 0; n <= 98; n++)
+	{
+		return (n);
+	}
+}
