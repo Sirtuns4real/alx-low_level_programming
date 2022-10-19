2a8bc4641993e5dc5ba30f48b47212326f047a03
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-19 09:39:04 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			printf("%d, ", n)
+			printf("%d, ", n);
 		}
 	}
 	else
@@ -22,7 +22,7 @@ void print_to_98(int n)
 		for (n = n; n > 98; n--)
 
 		{
-			printf("%d, ", n)
+			printf("%d, ", n);
 		}
 	}
 }
