262c08e8ff34e4e2ec429883c86db88936475b84
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-24 09:17:55 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts2 - function that prints a string, followed by a new line, to stdout
+* @str: pointer value
+* Return: Always 0 (Success)
+*/
+
+void _puts2(char *str)
+{
+	int i;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		 i++;
+	}
+
+	_putchar('\n');
+}
