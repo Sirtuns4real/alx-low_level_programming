58d7c83fcf60ac0a16a1208f0f93cad7982eaab2
sirtuns4real <olatundeoloyede41@yahoo.com>
2022-10-24 07:56:15 -0700

    new

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,13 @@
 
 void _puts(char *str)
 {
-  int i = 0;
-  
-  while (str[i] != '\0')
-  {
-    _putchar(str[i]);
-    i++;
-  }
-  _putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		 _putchar(str[i]);
+		 i++;
+	}
+
+	_putchar('\n');
 }
