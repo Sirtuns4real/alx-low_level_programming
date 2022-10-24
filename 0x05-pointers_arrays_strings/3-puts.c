1f58f53038062a395db32181ed2c5281a5270092
Olatunde Oloyede <114403043+Sirtuns4real@users.noreply.github.com>
2022-10-24 15:24:50 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - function that prints a string, followed by a new line, to stdout
+* @str: pointer value
+* Return: Always 0 (Success)
+*/
+
+void _puts(char *str)
+{
+  int i = 0;
+  
+  while (str[i] != '\0')
+  {
+    _putchar(str[i]);
+    i++;
+  }
+  _putchar('\n');
+}
