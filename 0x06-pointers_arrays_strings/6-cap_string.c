6c38a42d6266e57d496fb490581a8e3c0183bda7
Olatunde Oloyede <114403043+Sirtuns4real@users.noreply.github.com>
2022-10-26 17:07:43 +0100

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes every first letter of a word in a string
+ * separators of words are: space, tabulation
+ * new line
+ * @s: POinter to string
+ *
+ * Return: Pointer to s
+ */
+char *cap_string(char *s)
+{
+	int count;
+
+	count = 0;
+  while (s[count] != '\0')
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
