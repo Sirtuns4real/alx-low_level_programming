a89589948636b32252f8037dae9c906bf56da89b
Olatunde Oloyede <114403043+Sirtuns4real@users.noreply.github.com>
2022-11-07 15:03:29 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@ include <stdio.h>
  * @argv: Arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
