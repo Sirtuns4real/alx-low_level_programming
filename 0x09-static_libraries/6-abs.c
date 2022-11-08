a30d8f3ee63131751e5e6cf2e7b289fb31b89708
Olatunde Oloyede <114403043+Sirtuns4real@users.noreply.github.com>
2022-11-08 18:38:53 +0100

    Task 1

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: THe int to be operated upon
+ * Return: returns an unsigneed int value to calling function
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
