1c6e171a735c965aabb9cff9acfc95027cf28c5f
Olatunde Oloyede <114403043+Sirtuns4real@users.noreply.github.com>
2022-11-08 18:16:34 +0100

    Task 1

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Function for counting the length of string
+ * @s: a pointer for counting the strting length
+ * Return: int i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
