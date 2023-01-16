0d8f15e2d1b6dcd07c399ceac22aacd2fb58070e
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-01-16 09:58:07 +0100

    All C files

---
+++ 0x18-dynamic_libraries/_abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  * _abs - computes the absolute value of an integer
+ *   * @n: The int to be operated upon
+ *     * Return: returns an unsinged int value to calling function
+ *      */
+int _abs(int n)
+{
+		if (n > 0)
+					return (n);
+
+			else
+					{
+								n *= -1;
+										return (n);
+											}
+}
---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * _puts - prints a string to stdout
+ *   * @str: pointer parameter
+ *     */
+
+void _puts(char *str)
+{
+		int i = 0;
+
+			while (str[i] != '\0')
+					{
+								_putchar(str[i]);
+										i++;
+											}
+
+				_putchar('\n');
+}
